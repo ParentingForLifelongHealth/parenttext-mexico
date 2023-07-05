aa5cdc4aa2d5a198bb69e6abf63609231171690a
Ed Moss <ed.moss@eemengineering.com>
2023-07-05 13:02:27 -0300

    updating config

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 sources = [
-    {"filename": "parenttext_onboarding", "spreadsheet_id": "12ddvTz_ZfC-9-b0yxjVrSzczciUUE3GosLUFeOLIv9I", "crowdin_name": "onboarding"}
+    {"filename": "parenttext_onboarding", "spreadsheet_id": "12ddvTz_ZfC-9-b0yxjVrSzczciUUE3GosLUFeOLIv9I", "crowdin_name": "onboarding"},
 ]
+
+#Data copied from Chiara's pipeline for testing on multiple files
+
+# sources = [
+#     {"filename": "parenttext_onboarding", "spreadsheet_id": "12ddvTz_ZfC-9-b0yxjVrSzczciUUE3GosLUFeOLIv9I", "crowdin_name": "onboarding"},
+#     {"filename": "parenttext_modules", "spreadsheet_id": "1ONmD9PF9rcno3ha3QpfrIR5EIvHuuEqJXF3T90rlZ78", "crowdin_name": "modules_teen"},
+#     {"filename": "parenttext_goalcheckin", "spreadsheet_id": "1gympuD5KdlAdDJSuaVQiXjWSwJxoDcA9K-oBRyKmS7o", "crowdin_name": "goal_checkins"},
+#     {"filename": "parenttext_dev_assess_tools", "spreadsheet_id": "1OhhQF5yarUDmaSl2tlt7eIT7wJ8bGwNFzI3BOplJYsc", "crowdin_name": "dev_assess_tools"},
+#     {"filename": "parenttext_ltp_act_teen", "spreadsheet_id": "1Jx7vOmdefzK62ao2nlJJVLMAIS8d-6r1G8qn0jG8gww", "crowdin_name": "ltp_activities_teen"}
+# ]
 
 ab_testing_sheet_ID = "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s"
 
 languages = [
     {"language": "swa", "code": "ss"}
 ]
+
+translation_repo = ""
