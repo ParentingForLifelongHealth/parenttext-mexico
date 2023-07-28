ddba389552a56823c6c209a2f1514568ecbeabbc
fagiothree <32491856+fagiothree@users.noreply.github.com>
2023-07-28 09:44:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -19,15 +19,15 @@ safeguarding = "1PHgUhJnZdE0lK6C9teK-hwA6Tf-6Pgj1_OVdxoTgVOA"
 # tags are used to identify flows to be process
 # "split_no" is used to divide the file at the final step to get it to a manageable size that can be uploaded to rapidpro
 sources = [
-    {"filename": "parenttext_onboarding",
+    {"filename": "parenttext_menu",
      #"spreadsheet_ids": [T_C_onboarding_ID, T_content_ID,localised_sheets,C_ltp_activities_ID,T_delivery_ID],
      # Should be able to add the full list of sheets here but I was running into some sheet permission errors, hopefully will run ok on Chiara's account      
      "spreadsheet_ids": [localised_sheets, T_C_onboarding_ID, T_content_ID, C_ltp_activities_ID, T_delivery_ID, C_modules_teen_ID, C_dictionaries_ID, C_home_activity_checkin_ID, T_C_menu_ID, C_goal_checkin_ID, C_dev_asess_tool_ID, safeguarding], 
      "crowdin_name": "text_for_translators",
      # possible values for tag 1: onboarding dev_assess ltp_activity home_activity_checkin module goal_checkin safeguarding menu delivery
      #"tags": [1, "menu", 1, "onboarding", 2,"south_africa"],
-     "tags": [2,"south_africa"],
-     "split_no": 2},
+     "tags": [1, "menu",1, "delivery",2,"south_africa"],
+     "split_no": 1},
 ]
 
 # Model that is used as part of the process when the data is extracted from excel
@@ -67,7 +67,7 @@ special_words = "./edits/special_words.json"
 # In places where the QR are too long. We will make the changes to make the QRs numbers and add the number references to the message text as example 1.
 # Thresholds should be entered as strings
 count_threshold = "4"
-length_threshold = "6"
+length_threshold = "10"
 
 #Google sheet ID containing ab testing data
 ab_testing_sheet_ID = "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s" #same for all deployments
