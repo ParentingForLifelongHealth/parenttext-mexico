3e90945e643326a11800d4356f3f34f1e21c629e
Ed Moss <ed.moss@eemengineering.com>
2023-07-28 14:41:25 +0100

    Adding running note

--- process_safeguarding_words.py
+++ process_safeguarding_words.py
@@ -1,15 +1,13 @@
 import os
 import subprocess
 import sys
-# sys.path.append('C:/Users/edmun/Code/parenttext-pipeline/src/parenttext_pipeline')
-# from extract_keywords import process_keywords
-# leave in place for now, above code enables quick local testing
 
 from parenttext_pipeline.extract_keywords import process_keywords_to_file
 from config import *
 
 def main():
 
+    # Make sure the 3 letter code here aligns with the languages specified in the config file otherwise the safeguarding process will not work properly
     sources = [
         {"key": "zul", "path": "./excel_files/safeguarding zulu.xlsx"},
         {"key": "ssw", "path": "./excel_files/safeguarding swati.xlsx"}
